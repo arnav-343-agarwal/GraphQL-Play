c87a889c55d3afb31963508dee28f5ce6a524221
Arnav Agarwal <agarwalarnav2003@gmail.com>
2025-06-07 08:25:05 +0530

    start server config basic

--- server/index.js
+++ server/index.js
@@ -3,3 +3,18 @@ const {ApolloServer} = require('@apollo/server')
 const {expressMiddleware} = require('@apollo/server/express4')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+async function startServer() {
+    const app = express()
+    const server = ApolloServer({})
+
+    app.use(bodyParser.json())
+    app.use(cors())
+
+    await server.start()
+    app.use('/graphql',expressMiddleware(server))
+
+    app.listen(8000,()=>{
+        console.log("Server started at port 8000")
+    })
+}
