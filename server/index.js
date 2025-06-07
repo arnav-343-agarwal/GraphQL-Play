89523f50f09d4ac7b39daf96615c563652452882
Arnav Agarwal <agarwalarnav2003@gmail.com>
2025-06-07 08:53:08 +0530

    Making graphql studio work

--- server/index.js
+++ server/index.js
@@ -3,18 +3,40 @@ const {ApolloServer} = require('@apollo/server')
 const {expressMiddleware} = require('@apollo/server/express4')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const {
+  ApolloServerPluginLandingPageLocalDefault
+} = require('@apollo/server/plugin/landingPage/default');
 
 async function startServer() {
     const app = express()
-    const server = ApolloServer({})
+    const server = new ApolloServer({
+        typeDefs:`
+            type Todo {
+                id: ID!
+                title: String!
+                completed: Boolean
+            }
 
-    app.use(bodyParser.json())
+            type Query {
+                getTodos : [Todo]
+            }
+        `,
+        resolvers:{},
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    })
+
     app.use(cors())
+    app.use(express.json());
 
     await server.start()
-    app.use('/graphql',expressMiddleware(server))
+    app.get('/graphql', (req, res) => {
+        res.redirect('https://studio.apollographql.com/sandbox/explorer');
+    });
+    app.use('/graphql', express.json() ,expressMiddleware(server))
 
     app.listen(8000,()=>{
         console.log("Server started at port 8000")
     })
 }
+
+startServer()
